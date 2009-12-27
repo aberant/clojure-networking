f042ec77f862dd6eac977212a7099aa4eb6f6892
aberant <qzzzq1@gmail.com>
2009-12-27 16:36:49 -0600

    code cleanup

--- server/udp.clj
+++ server/udp.clj
@@ -1,26 +1,22 @@
-(import '(java.io IOException)
-        '(java.net InetAddress DatagramPacket DatagramSocket SocketException))
-
-(defn localhost [] (. InetAddress getLocalHost))
+(import '(java.net InetAddress DatagramPacket DatagramSocket))
 
 (def udp-server (ref nil))
 
+(def port 3333)
+
+(defn localhost [] (. InetAddress getLocalHost))
+
 (defn message [text]
-  (new DatagramPacket (. text getBytes) (. text length) (localhost) 3333))
+  (new DatagramPacket (. text getBytes) (. text length) (localhost) port ))
 
 (defn send-message [text]
-  (. send udp-server (message text)))
+  (.send @udp-server (message text)))
 
 (defn create-udp-server []
-  (DatagramSocket. 3333))
+  (DatagramSocket. port))
 
 (defn start-udp-server []
      (dosync (ref-set udp-server (create-udp-server))))
 
 (defn stop-udp-server []
      (.close @udp-server))
-
-(defn test-crap []
-  (start-udp-server)
-  (send-message "hello")
-  (stop-udp-server))
